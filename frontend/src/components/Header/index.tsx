45c915ac3353ff217e768b9756163d85a3000f3a
Vinicius Jara <viniciusfjara@gmail.com>
2023-04-13 16:17:22 -0400

    Other Components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from "../../assets/img/logo.svg";
+import "./styles.css";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="dsmeta-logo-container">
+        <img src={logo} alt="DSMeta" />
+        <h1>DSMeta</h1>
+        <p>
+          Desenvolvido por
+          <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+        </p>
+      </div>
+    </header>
+  );
+}
